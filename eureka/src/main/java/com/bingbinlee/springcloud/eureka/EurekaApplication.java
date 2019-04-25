cea587bb1eec3589276c4c307a9c671464ea46dd
bingbin.lee <bingbin.lee@foxmail.com>
2019-04-25 17:39:03 +0800

    RA-Eureka

---
+++ eureka/src/main/java/com/bingbinlee/springcloud/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.bingbinlee.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 使用Eureka做注册中心
+ * @author bingbin.lee
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaApplication.class, args);
+  }
+}
